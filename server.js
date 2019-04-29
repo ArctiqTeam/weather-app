e21b71e889d02af0a35f10f099a34f7b0d8e1ccb
Roman Romanenko <35006889+roman-in-arctiq@users.noreply.github.com>
2019-04-29 13:50:58 -0400

    Added functionality for map modes.

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var hostname        = os.hostname();
 
 const weatherApiKey = '6fff6aaa446248276619c698beb22f3d'; // https://openweathermap.org/api
 const mapApiKey     = 'Gxxyg7GH2PBHYYZvZwsABn7kldNMTGl1'; // https://developer.mapquest.com/
-const mapType       = 'sat';                              // map, hyb, sat, light, dark
+
+var mapType         = 'sat';                              // map, hyb, sat, light, dark
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,15 +21,17 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`;
-
   request(url, function (err, response, body) {
     if(err){
       res.render('index', {hostname: hostname, weather: null, error: 'Error, please try again...'});
     } else {
-      let weather = JSON.parse(body)
+      let weather = JSON.parse(body);
       if(weather.main == undefined){
         res.render('index', {hostname: hostname, weather: null, error: 'Error, please try again...'});
       } else {
+        if(typeof req.query.map != 'undefined') {
+          mapType = req.query.map;
+        }
         res.render('index', {hostname: hostname, weather: weather, mapApiKey: mapApiKey, mapType: mapType, error: null});
       }
     }
