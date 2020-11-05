51d114b5f6be9155d2ab4189ae0476e05514cde0
Daniyal <daniyaljavedk@gmail.com>
2020-11-04 21:55:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
   request(url, function (err, response, body) {
     if(err){
@@ -28,7 +28,7 @@ app.post('/', function (req, res) {
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees farenheit in ${weather.name}!`;
+        let weatherText = `It's ${weather.main.temp} degrees celsius in ${weather.name}!`;
         res.render('index', {weather: weatherText, error: null, hostname: hostname});
       }
     }
